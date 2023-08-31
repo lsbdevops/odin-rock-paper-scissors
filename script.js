8af4ea3feb8edc7ef67f33edd1645a206f099e1b
lsbdevops <lsbdevops@gmail.com>
2023-08-31 22:40:55 +1000

    Add score tally and declaration of overall winner
    scoreUpdate function created to increment the score tally
    of the round winner and display the updated score on the
    webpage. Function also checks if there is an overall winner
    (i.e., score equal to WINNING_SCORE constant) and announces
    the winner. Once an overall winner is determined, the game
    is stopped by removal of the event listeners.

--- script.js
+++ script.js
@@ -14,25 +14,79 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     // To ensure case-insensitivity, convert the string stored in the playerSelection variable to lower case.
     playerSelection = playerSelection.toLowerCase();
+    
+    const result = {"result": "",
+                    "winner": ""};
 
     // Check each win and lose condition for "rock, paper, scissors" and return the winner in a string.
-    if ((playerSelection === "rock") && (computerSelection === "scissors")) return "You win! Rock beats Scissors.";
-    else if ((playerSelection === "rock") && (computerSelection === "paper")) return "You lose! Paper beats Rock.";
-    else if ((playerSelection === "scissors") && (computerSelection === "rock")) return "You lose! Rock beats Scissors.";
-    else if ((playerSelection === "scissors") && (computerSelection === "paper")) return "You win! Scissors beats Paper.";
-    else if ((playerSelection === "paper") && (computerSelection === "scissors")) return "You lose! Scissors beats Paper.";
-    else if ((playerSelection === "paper") && (computerSelection === "rock")) return "You win! Paper beats Rock.";
-
+    if (((playerSelection === "rock") && (computerSelection === "scissors")) || 
+        ((playerSelection === "scissors") && (computerSelection === "paper")) ||
+        ((playerSelection === "paper") && (computerSelection === "rock")))
+    {
+    result.result = `You win! ${playerSelection} beats ${computerSelection}.`;
+    result.winner = "player";
+    }
+    else if (((playerSelection === "rock") && (computerSelection === "paper")) ||
+        ((playerSelection === "scissors") && (computerSelection === "rock")) ||
+        ((playerSelection === "paper") && (computerSelection === "scissors")))
+    {
+    result.result = `You Lose! ${playerSelection} beats ${computerSelection}.`;
+    result.winner = "computer";
+    }
     // If no win conditions are met, then the results is a draw.
-    else return "Draw!";
+    else{
+    result.result = "Draw!";
+    result.winner = "draw";  
+    };
+
+    return result;
 }
 
 // Function that plays a game of rock, paper, scissors by getting the user input and playing a round.
-function game() {
-    // Prompt the user for their choice of rock, paper or scissors and store in a variable.
-    const playerChoice = prompt("Rock, paper or scissors?");
+function game(event) {
+    // Get the user's selection from the button clicked (the button's id).
+    const playerChoice = this.id;
 
     // Call the playRound function with inputs of the user's choice variable and output of the getComputerChoice function.
-    console.log(playRound(playerChoice, getComputerChoice()));
+    // Print result on the webpage.
+    const resultElement = document.querySelector("#result")
+    const result = playRound(playerChoice, getComputerChoice())
+    resultElement.textContent = "Round result: " + result.result;
+
+    // Update the score and check if there's an overall winner.
+    if (scoreUpdate(result.winner))
+    {
+        document.querySelector("#winner").textContent = `${result.winner} wins!`
+        buttons.forEach((button) => {
+            button.removeEventListener("click", game);
+         })
+    }
+    else return;
 }
 
+function scoreUpdate(winner) {
+    // Check the winner and get the appropriate score element.
+    const scoreElement = (winner === "player") ?  document.querySelector("#players-score")
+    : (winner === "computer") ? document.querySelector("#computers-score")
+    : null;
+
+    // If a draw, do not update the score.
+    if (!scoreElement) return;
+
+    // Increment the current score by 1.
+    let score = +(scoreElement.textContent) + 1;
+    scoreElement.textContent = score;
+
+    // Check for a winner.
+    if (score === WINNING_SCORE) return true;
+    else return false;
+}
+
+const WINNING_SCORE = 5;
+
+// Add an event listener to all buttons, and run game function when clicked.
+const buttons = document.querySelectorAll("button");
+
+ buttons.forEach((button) => {
+    button.addEventListener("click", game);
+ })
